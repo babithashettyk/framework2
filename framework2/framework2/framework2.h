7288163660f9758ea29c0470e17f935ce1722cfb
Babitha Shetty K <babitha.shetty@globaldelight.com>
2021-11-12 09:23:19 +0530

    framework added

---
+++ framework2/framework2/framework2.h
@@ -0,0 +1,18 @@
+//
+//  framework2.h
+//  framework2
+//
+//  Created by Babitha Shetty K on 12/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for framework2.
+FOUNDATION_EXPORT double framework2VersionNumber;
+
+//! Project version string for framework2.
+FOUNDATION_EXPORT const unsigned char framework2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <framework2/PublicHeader.h>
+
+
---
+++ framework2/framework2/swiftClass.swift
@@ -0,0 +1,13 @@
+//
+//  swiftClass.swift
+//  framework2
+//
+//  Created by Babitha Shetty K on 12/11/21.
+//
+
+import Foundation
+public class swiftClass{
+    public class func one(){
+        
+    }
+}
